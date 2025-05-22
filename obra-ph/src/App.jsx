a84ec55beb3d564a6a4ccf4fbf4524888f58cd2e
chippee08 <paolobanag08@gmail.com>
2025-05-22 17:15:44 +0800

    login
    gumawa login form

--- obra-ph/src/App.jsx
+++ obra-ph/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Login from './component/Login'
 
 function App() {
   return (
-    <div className='bg-red-700 text-white'>App</div>
+    <>
+    <div className='bg-[#d9d9d9] h-screen flex justify-center items-center'>
+      <Login />
+    </div>
+    
+    </>
   )
 }
 
--- obra-ph/src/component/Login.jsx
+++ obra-ph/src/component/Login.jsx
@@ -2,7 +2,37 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <>
+      <div className="flex bg-white items-center justify-center border-2 border-gray-300 rounded-lg shadow-lg w-[800px] h-[400px]">
+        {/* Login Form */}
+        <div className='flex flex-col justify-center items-center p-8'>
+          <h1 className="text-2xl mb-4">Sign in</h1>
+          <form>
+            <div className="mb-4">
+              <label htmlFor="email" className="block mb-1">Email</label>
+              <input type="text" id="email" name="email" className="border-2 border-gray-300 rounded-lg shadow-lg p-2 w-full" />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="password" className="block mb-1">Password</label>
+              <input type="password" id="password" name="password" className="border-2 border-gray-300 rounded-lg shadow-lg p-2 w-full" />
+            </div>
+            <div className='flex flex-col items-center'>
+              <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Login</button>
+              <p>No account yet?</p>
+              <a href="" className=''>Sign up</a>
+            </div>
+          </form>
+        </div>
+
+        {/* Divider Line */}
+        <div className="w-px h-64 bg-gray-300 mx-8" />
+
+        {/* Image */}
+        <div className='p-8 flex justify-center items-center'>
+          <img src="src/assets/header-logo.png" alt="Header Logo" className="h-[200px]" />
+        </div>
+      </div>
+    </>
   )
 }
 

3c2a400c314bda95d53c8bf2d373e1ee3f9d0aa0
chippee08 <paolobanag08@gmail.com>
2025-05-23 17:11:37 +0800

    Update Login.jsx

--- obra-ph/src/component/Login.jsx
+++ obra-ph/src/component/Login.jsx
@@ -6,20 +6,24 @@ function Login() {
       <div className="flex bg-white items-center justify-center border-2 border-gray-300 rounded-lg shadow-lg w-[800px] h-[400px]">
         {/* Login Form */}
         <div className='flex flex-col justify-center items-center p-8'>
-          <h1 className="text-2xl mb-4">Sign in</h1>
+          <h1 className="text-2xl mb-4 text-[#595F3B]">Sign in</h1>
           <form>
             <div className="mb-4">
-              <label htmlFor="email" className="block mb-1">Email</label>
+              <label htmlFor="email" className="block mb-1 text-[#595F3B]">Email</label>
               <input type="text" id="email" name="email" className="border-2 border-gray-300 rounded-lg shadow-lg p-2 w-full" />
             </div>
             <div className="mb-4">
-              <label htmlFor="password" className="block mb-1">Password</label>
+              <label htmlFor="password" className="block mb-1 text-[#595F3B]">Password</label>
               <input type="password" id="password" name="password" className="border-2 border-gray-300 rounded-lg shadow-lg p-2 w-full" />
             </div>
+            <div className="mb-4">
+              <input type="checkbox" id="remember" name="remember" className="mr-2" />
+              <label htmlFor="remember" className="text-[#595F3B]">Remember me</label>
+            </div>
             <div className='flex flex-col items-center'>
-              <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Login</button>
+              <button type="submit" className="bg-[#595F3B] text-white px-4 py-2 rounded">Login</button>
               <p>No account yet?</p>
-              <a href="" className=''>Sign up</a>
+              <a href="" className='text-[#595F3B]'><u>Sign up</u></a>
             </div>
           </form>
         </div>

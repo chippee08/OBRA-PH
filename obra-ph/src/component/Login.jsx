9862b382a3dcb1405a9ac30e6ca70801e3cd1fe5
chippee08 <paolobanag08@gmail.com>
2025-05-23 17:40:02 +0800

    Update Login.jsx

--- obra-ph/src/component/Login.jsx
+++ obra-ph/src/component/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Login() {
   return (
     <>
-      <div className="flex bg-white items-center justify-center border-2 border-gray-300 rounded-lg shadow-lg w-[800px] h-[400px]">
+      <div className="flex bg-white items-center justify-center border-2 border-gray-300 rounded-lg shadow-lg w-[1000px] h-[400px]">
         {/* Login Form */}
         <div className='flex flex-col justify-center items-center p-8'>
           <h1 className="text-2xl mb-4 text-[#595F3B]">Sign in</h1>
@@ -15,6 +15,7 @@ function Login() {
             <div className="mb-4">
               <label htmlFor="password" className="block mb-1 text-[#595F3B]">Password</label>
               <input type="password" id="password" name="password" className="border-2 border-gray-300 rounded-lg shadow-lg p-2 w-full" />
+              <a href="" className='text-[#595F3B]'><u>Forgot password?</u></a>
             </div>
             <div className="mb-4">
               <input type="checkbox" id="remember" name="remember" className="mr-2" />
